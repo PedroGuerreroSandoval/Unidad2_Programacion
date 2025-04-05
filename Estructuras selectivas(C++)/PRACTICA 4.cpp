84e0c924ce1ca9fc00d293b89f5bd25467618ef6
PedroGuerreroSandoval <pedroguerrerosandoval7805@gmail.com>
2025-04-04 23:12:43 -0500

    Add files via upload

---
+++ Estructuras selectivas(C++)/PRACTICA 4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Mensaje de bienvenida
+	cout << "Hola :) Este programa 2 unidad 2" << "\n";
+	
+	//Se declaran los numeros que se sumaran (Pueden ser decimales)
+	float SUELDO, aum, nsue;
+	
+	//Se pide el primer numero
+	cout << "Por favor ingrese el sueldo:  ";
+	
+	//Se asigna el primer valor a SUELDO
+	cin >> SUELDO;
+	
+if (SUELDO < 400000.00)
+      {
+       aum = SUELDO * 0.15;
+       nsue = SUELDO + aum;
+        }
+    else
+        nsue=SUELDO*1.08;
+        
+          
+	cout << "El nuevo sueldo es:  "<<nsue ;
+    //Se muestra resultado
+    
+    return 0;
+}
