3a1fc5abfeb7cd704553bcc5fde2f20e3f50e905
PedroGuerreroSandoval <pedroguerrerosandoval7805@gmail.com>
2025-04-04 23:18:00 -0500

    Add files via upload

---
+++ Practicas de For While Do/Practica 1/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(77, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(354, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "Escribe un numero del 0 al 10"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 25
+        ListBox1.Location = New Point(100, 87)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(286, 479)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveBorder
+        Button1.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(466, 218)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(143, 42)
+        Button1.TabIndex = 2
+        Button1.Text = "Listar"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(697, 643)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Practicas de For While Do/Practica 1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Integer = 0
+
+        Do
+            ListBox1.Items.Add(x)
+            x += 1
+        Loop While x <= 10
+    End Sub
+End Class
